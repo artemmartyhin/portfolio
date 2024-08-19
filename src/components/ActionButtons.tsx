fd9086a77bb38cd714490de271a2aa7e6f7727a5
artemmartyhin <artemmartyhin@gmail.com>
2024-08-19 21:07:25 +0000

    fix: mobile optimization

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -9,29 +9,30 @@ const ActionButtons: FunctionComponent<ActionButtonsType> = ({
 }) => {
   return (
     <div
-      className={`flex flex-row items-start justify-start gap-[18px] max-w-full mq750:flex-wrap ${className}`}
+      className={`flex flex-wrap items-center justify-center gap-[18px] w-full ${className}`}
     >
       <a
-        href="https://www.linkedin.com/in/temmartik/" // Replace with your LinkedIn URL
-        target="_blank" // Opens the link in a new tab
-        rel="noopener noreferrer" // Security best practice when using target="_blank"
-        className="cursor-pointer [border:none] py-4 px-7 bg-[transparent] flex flex-row items-start justify-start relative  [text-decoration:none]"
+        href="https://www.linkedin.com/in/temmartik/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="cursor-pointer [border:none] py-4 px-7 bg-[transparent] flex items-center justify-center [text-decoration:none]"
       >
-        <button className="cursor-pointer [border:none] py-4 pl-[30px] pr-[29px] bg-[transparent] flex flex-row items-start justify-start relative whitespace-nowrap">
-          <div className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] rounded-31xl [background:linear-gradient(#fff,_#fff),_#fff] border-primary-color border-[1px] border-solid box-border" />
-          <div className="relative text-2xl-4 font-semibold font-poppins text-background-color text-left [text-shadow:0.3px_0_0_#fff,_0_0.3px_0_#fff,_-0.3px_0_0_#fff,_0_-0.3px_0_#fff] z-[1]">
+        <button className="cursor-pointer [border:none] py-4 px-[30px] bg-[transparent] flex items-center justify-center relative whitespace-nowrap">
+          <div className="absolute inset-0 rounded-31xl border-primary-color border-[1px] box-border bg-white" />
+          <div className="relative text-2xl-4 font-semibold font-poppins text-background-color [text-shadow:0.3px_0_0_#fff,_0_0.3px_0_#fff,_-0.3px_0_0_#fff,_0_-0.3px_0_#fff] z-[1]">
             Get In Touch
           </div>
         </button>
       </a>
       <a
-        href="/cvl.pdf" // The file to download
-        download="Artem_Martiukhin_CV.pdf" // The file name
-        className="cursor-pointer [border:none] py-4 px-7 bg-[transparent] flex flex-row items-start justify-start relative  [text-decoration:none]"
+        href="/cvl.pdf"
+        download="Artem_Martiukhin_CV.pdf"
+        className="cursor-pointer [border:none] py-4 px-7 bg-[transparent] flex items-center justify-center [text-decoration:none]"
       >
-        <button className="cursor-pointer [border:none] py-4 pl-[30px] pr-[29px] bg-[transparent] flex flex-row items-start justify-start relative whitespace-nowrap">
-          <div className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] rounded-31xl border-primary-color border-[2px] border-solid box-border" />
-          <div className="relative text-2xl-4 font-semibold font-poppins text-primary-color text-left [text-shadow:0.3px_0_0_#ff8660,_0_0.3px_0_#ff8660,_-0.3px_0_0_#ff8660,_0_-0.3px_0_#ff8660] z-[1] mq450:text-mid">
+        <button className="cursor-pointer [border:none] py-4 px-[30px] bg-[transparent] flex items-center justify-center relative whitespace-nowrap">
+          <div className="absolute inset-0 rounded-31xl border-primary-color border-[2px] box-border border-solid" />
+
+          <div className="relative text-2xl-4 font-semibold font-poppins text-primary-color [text-shadow:0.3px_0_0_#ff8660,_0_0.3px_0_#ff8660,_-0.3px_0_0_#ff8660,_0_-0.3px_0_#ff8660] z-[1] mq450:text-mid">
             Download CV
           </div>
         </button>
--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -6,146 +6,41 @@ export type LogosType = {
 
 const Logos: FunctionComponent<LogosType> = ({ className = "" }) => {
   return (
-    <div
-      className={`flex flex-col items-center justify-center gap-8 ${className}`}
-    >
+    <div className={`flex flex-col items-center justify-center gap-8 ${className}`}>
       {/* Programming Languages */}
-      <div className="flex flex-row items-center justify-center gap-5">
-        <img
-          className="h-[45px] w-[45px] relative"
-          alt=""
-          src="/solidity.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/typescript.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/javascript.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/c.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/r.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/go.svg"
-        />
+      <div className="flex flex-wrap items-center justify-center gap-5">
+        <img className="h-[45px] w-[45px]" alt="Solidity" src="/solidity.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="TypeScript" src="/typescript.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="JavaScript" src="/javascript.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="C" src="/c.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="R" src="/r.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="Go" src="/go.svg" />
       </div>
 
       {/* Frameworks */}
-      <div className="flex flex-row items-center justify-center gap-5">
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/nodejs.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/nestjs.svg"
-        />
-
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/react.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/hardhat.svg"
-        />
-        <img
-          className="h-[40px] w-[40px] relative"
-          loading="lazy"
-          alt=""
-          src="/cmake.svg"
-        />
-        <img
-          className="h-[48px] w-[48px] relative"
-          loading="lazy"
-          alt=""
-          src="/docker.svg"
-        />
-
-        <img
-          className="h-[40px] w-[42px] relative"
-          loading="lazy"
-          alt=""
-          src="/git.svg"
-        />
+      <div className="flex flex-wrap items-center justify-center gap-5">
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="Node.js" src="/nodejs.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="NestJS" src="/nestjs.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="React" src="/react.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="Hardhat" src="/hardhat.svg" />
+        <img className="h-[40px] w-[40px]" loading="lazy" alt="CMake" src="/cmake.svg" />
+        <img className="h-[48px] w-[48px]" loading="lazy" alt="Docker" src="/docker.svg" />
+        <img className="h-[40px] w-[42px]" loading="lazy" alt="Git" src="/git.svg" />
       </div>
 
       {/* Blockchain Networks */}
-      <div className="flex flex-row items-center justify-center gap-5">
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/ethereum.svg"
-        />
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/bitcoin.svg"
-        />
-        <img
-          className="h-[41px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/ton.svg"
-        />
-        <img
-          className="h-[41px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/graph.svg"
-        />
+      <div className="flex flex-wrap items-center justify-center gap-5">
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="Ethereum" src="/ethereum.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="Bitcoin" src="/bitcoin.svg" />
+        <img className="h-[41px] w-[45px]" loading="lazy" alt="TON" src="/ton.svg" />
+        <img className="h-[41px] w-[45px]" loading="lazy" alt="The Graph" src="/graph.svg" />
       </div>
 
       {/* Databases */}
-      <div className="flex flex-row items-center justify-center gap-5">
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/mongo.svg"
-        />
-
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/postgresql.svg"
-        />
-
-        <img
-          className="h-[45px] w-[45px] relative"
-          loading="lazy"
-          alt=""
-          src="/redis.svg"
-        />
+      <div className="flex flex-wrap items-center justify-center gap-5">
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="MongoDB" src="/mongo.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="PostgreSQL" src="/postgresql.svg" />
+        <img className="h-[45px] w-[45px]" loading="lazy" alt="Redis" src="/redis.svg" />
       </div>
     </div>
   );
